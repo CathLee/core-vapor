f4cb798fcbc0e54e426514c2bcce2bf91f409963
CathLee <447932704@qq.com>
2024-03-25 23:27:56 +0800

    test:test close tag

--- packages/compiler-vapor/__tests__/compile.spec.ts
+++ packages/compiler-vapor/__tests__/compile.spec.ts
@@ -27,6 +27,17 @@ describe('compile', () => {
     expect(code).matchSnapshot()
     expect(code).contains(JSON.stringify('<div><span><div /></span></div>'))
   })
+  test('two close tag ', () => {
+    const code = compile(`<div><span></span><span></span></div>`)
+    expect(code).matchSnapshot()
+    expect(code).contains(JSON.stringify('<div><span /><span /></div>'))
+  })
+
+  test('two close tag with text', () => {
+    const code = compile(`<div><span>ddd</span><span></span></div>`)
+    expect(code).matchSnapshot()
+    expect(code).contains(JSON.stringify('<div><span>ddd</span><span /></div>'))
+  })
 
   test('dynamic root', () => {
     const code = compile(`{{ 1 }}{{ 2 }}`)
